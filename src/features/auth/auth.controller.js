0cb520ee604fed6ef599318207c9459212fef302
Abisoye Ogunmona <Ogunmonaa@gmail.com>
2025-08-06 16:58:16 +0100

    feat: implement authentication functionality
    - Added auth controller, routes, services, and middleware
    - Integrated auth validator with Joi for request validation
    - Implemented password rules (uppercase, lowercase, number, symbol, min length, spaces allowed)
    - Updated protectRoute middleware for improved token and user status checks
    - Added stream config for future integration of Stream users
    - Restored and tested catch-all route for unmatched API requests
    - Adjusted logger.js and validate.middleware for better error handling

---
+++ src/features/auth/auth.controller.js
@@ -0,0 +1,180 @@
+const sendResponse = require("../../shared/utils/sendResponse");
+const {
+  addStreamUser,
+  changeUserPassword,
+  createUser,
+  fetchProfile,
+  forgotUserPassword,
+  loginUser,
+  resendVerificationEmail,
+  resetUserPassword,
+  verifyUserEmail,
+} = require("./auth.service");
+const {
+  sendPasswordChangeSuccessEmail,
+  sendPasswordResetEmail,
+  sendResetSuccessEmail,
+  sendVerificationEmail,
+  sendWelcomeEmail,
+} = require("../../shared/mailtrap/emails");
+
+exports.signup = async (req, res, next) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+    const newUser = await createUser({
+      firstName,
+      lastName,
+      email,
+      password,
+    });
+
+    // await sendVerificationEmail(newUser.email, newUser.verificationToken);
+    // await addStreamUser(newUser);
+
+    const token = newUser.generateAuthToken();
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production",
+      // domain: ".edupeerhub.com",
+    });
+
+    sendResponse(res, 201, "User registered successfully", {
+      id: newUser.id,
+      email: newUser.email,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const user = await loginUser({ ...req.body });
+    const token = user.generateAuthToken();
+
+    user.lastLogin = new Date();
+    await user.save();
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production",
+      // domain: ".edupeerhub.com",
+    });
+
+    sendResponse(res, 200, "User signed in successfully", {
+      id: user.id,
+      email: user.email,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.profile = async (req, res, next) => {
+  try {
+    const user = await fetchProfile(req.user.id);
+    sendResponse(res, 200, "Profile fetch successful", user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.logout = (req, res, next) => {
+  res.clearCookie("jwt", {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    secure: process.env.NODE_ENV === "production",
+    // domain: ".edupeerhub.com",
+  });
+  sendResponse(res, 200, "Logout Successful");
+};
+
+exports.verifyEmail = async (req, res, next) => {
+  try {
+    const { code } = req.body;
+    const verifiedUser = await verifyUserEmail(code);
+
+    // await sendWelcomeEmail(verifiedUser.email, verifiedUser.fullName);
+    sendResponse(res, 200, "Email verified successfully", {
+      id: verifiedUser.id,
+      email: verifiedUser.email,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.resendEmail = async (req, res, next) => {
+  try {
+    const user = await resendVerificationEmail(req.user.id);
+    // await sendVerificationEmail(user.email, user.verificationToken);
+
+    sendResponse(res, 200, "Verification email resent successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.forgotPassword = async (req, res, next) => {
+  try {
+    const result = await forgotUserPassword(req.body.email);
+
+    // if (result) {
+    //   await sendPasswordResetEmail(
+    //     result.userEmail,
+    //     `${process.env.CLIENT_URL}/reset-password/${result.resetToken}`
+    //   );
+    // }
+
+    sendResponse(res, 200, "Password reset link sent to your email");
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.resetPassword = async (req, res, next) => {
+  try {
+    const { token } = req.params;
+    const email = await resetUserPassword(token, req.body.password);
+
+    // await sendResetSuccessEmail(email);
+    sendResponse(res, 200, "Password reset successful");
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.changePassword = async (req, res, next) => {
+  try {
+    const result = await changeUserPassword(
+      req.user.id,
+      req.body.currentPassword,
+      req.body.newPassword
+    );
+
+    // await sendPasswordChangeSuccessEmail(user.email);
+    sendResponse(res, 200, "Password changed successfully", {
+      id: result.id,
+      email: result.email,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// onboarding will be per user role of student or mentor
+// exports.onboard = async (req, res, next) => {
+//   try {
+//     const updatedUser = await onBoardUser({ userId: req.user.id, ...req.body });
+
+//     await addStreamUser(updatedUser);
+//     sendResponse(res, 200, "Onboard successful", updatedUser);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
--- src/features/auth/auth.route.js
+++ src/features/auth/auth.route.js
@@ -1,7 +1,70 @@
-// POST / api / auth / register; // Student/tutor registration
-// POST / api / auth / login; // Email/password login
-// POST / api / auth / logout; // Clear session
-// PUT / api / auth / me; // Edit student/tutor profile
-// POST / api / auth / forgot - password;
-// POST / api / auth / reset - password;
-// GET / api / auth / me; // Get current user info
+const express = require("express");
+const authController = require("./auth.controller");
+const validate = require("../../shared/middlewares/validate.middleware");
+const authValidation = require("./auth.validator");
+const { protectRoute } = require("./auth.middleware");
+const createRateLimiter = require("../../shared/middlewares/rateLimit.middleware");
+const rateLimitConfig = require("../../shared/config/rateLimit.config");
+
+const router = express.Router();
+
+// Public auth routes
+router.post(
+  "/signup",
+  createRateLimiter(rateLimitConfig.signup),
+  validate(authValidation.signup),
+  authController.signup
+);
+
+router.post(
+  "/login",
+  createRateLimiter(rateLimitConfig.login),
+  validate(authValidation.login),
+  authController.login
+);
+
+router.post(
+  "/forgot-password",
+  // createRateLimiter(rateLimitConfig.forgotPassword),
+  validate(authValidation.forgotPassword),
+  authController.forgotPassword
+);
+
+router.post(
+  "/reset/:token",
+  createRateLimiter(rateLimitConfig.resetPassword),
+  validate(authValidation.resetPassword),
+  authController.resetPassword
+);
+
+// Protected routes
+router.use(protectRoute);
+
+router.get("/me", authController.profile);
+router.post(
+  "/verify-email",
+  createRateLimiter(rateLimitConfig.verifyEmail),
+  validate(authValidation.verifyEmail),
+  authController.verifyEmail
+);
+
+router.post(
+  "/resend-email-verification",
+  createRateLimiter(rateLimitConfig.resendVerification),
+  authController.resendEmail
+);
+
+router.put(
+  "/change-password",
+  validate(authValidation.changePassword),
+  authController.changePassword
+);
+
+// router.post(
+//   "/onboarding",
+//   validate(authValidation.onboarding),
+//   authController.onboard
+// );
+router.post("/logout", authController.logout);
+
+module.exports = router;
