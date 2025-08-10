e80598f3188e4181a007b69bf0ec776c8eb3374f
Abisoye Ogunmona <Ogunmonaa@gmail.com>
2025-08-10 01:06:08 +0100

    fix: pgsql db prod config to use username and password login

--- src/shared/config/db.config.js
+++ src/shared/config/db.config.js
@@ -29,7 +29,10 @@ module.exports = {
     logging: false,
   },
   production: {
-    use_env_variable: "DATABASE_URL",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "postgres",
     logging: false,
   },
