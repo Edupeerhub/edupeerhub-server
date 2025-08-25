d808ba62b1f78741830c0d9fb1e8c0b4b40d4bd1
Abisoye Ogunmona <Ogunmonaa@gmail.com>
2025-08-25 02:32:18 +0100

    fix: add ssl for db config dev

--- src/shared/config/db.config.js
+++ src/shared/config/db.config.js
@@ -30,6 +30,12 @@ module.exports = {
           acquire: 30000,
           idle: 10000,
         },
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
         dialect: "postgres",
         logging: console.log,
       },
