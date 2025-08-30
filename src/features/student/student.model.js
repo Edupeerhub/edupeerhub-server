bbd85e0c832986c775e421a8512ca4a5c9c8514c
Abisoye Ogunmona <Ogunmonaa@gmail.com>
2025-08-30 01:54:30 +0100

    add student profile migration script

--- src/features/student/student.model.js
+++ src/features/student/student.model.js
@@ -12,7 +12,7 @@ module.exports = () => {
       },
       gradeLevel: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
       },
       learningGoals: {
         type: DataTypes.TEXT,
