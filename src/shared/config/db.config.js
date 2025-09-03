62e5f3292000029dd1e1d134f1689ef8cbdeee2f
Abisoye Ogunmona <Ogunmonaa@gmail.com>
2025-09-03 14:20:31 +0100

    feat: setup cookie domain config for prod

--- src/shared/config/db.config.js
+++ src/shared/config/db.config.js
@@ -41,10 +41,8 @@ module.exports = {
     logging: false,
   },
   staging: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
     dialectOptions: {
       ssl: {
         require: true,
