75ba6cf6db8f62272bf167536426cae57aa6113b
Abisoye Ogunmona <Ogunmonaa@gmail.com>
2025-09-08 19:40:56 +0100

    fix: duplicate seeder

--- src/shared/database/seeders/20250820155218-seed-exams.js
+++ src/shared/database/seeders/20250820155218-seed-exams.js
@@ -2,36 +2,40 @@
 
 module.exports = {
   async up(queryInterface) {
-    await queryInterface.bulkInsert("exams", [
-      {
-        name: "JAMB",
-        description: "For university entrance",
-        is_active: true,
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-      {
-        name: "WAEC",
-        description: "For Senior Secondary School Certificates",
-        is_active: true,
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-      {
-        name: "NABTEB",
-        description: "For technical and business exams",
-        is_active: true,
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-      {
-        name: "BECE",
-        description: "For Junior Secondary School completion",
-        is_active: true,
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-    ]);
+    await queryInterface.bulkInsert(
+      "exams",
+      [
+        {
+          name: "JAMB",
+          description: "For university entrance",
+          is_active: true,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: "WAEC",
+          description: "For Senior Secondary School Certificates",
+          is_active: true,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: "NECO",
+          description: "For Senior Secondary School Certificates",
+          is_active: true,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: "GCE",
+          description: "For Senior Secondary School Certificates",
+          is_active: true,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      { ignoreDuplicates: true }
+    );
   },
 
   async down(queryInterface) {
