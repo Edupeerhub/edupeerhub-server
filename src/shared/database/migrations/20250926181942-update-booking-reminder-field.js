1ad6d75337938aa8d30f60c501204b0a357f9c21
Abisoye Ogunmona <Ogunmonaa@gmail.com>
2025-09-27 03:42:51 +0100

    add index update to migration

--- src/shared/database/migrations/20250926181942-update-booking-reminder-field.js
+++ src/shared/database/migrations/20250926181942-update-booking-reminder-field.js
@@ -12,13 +12,50 @@ module.exports = {
         reminderSlot3: false,
       },
     });
+
+    // Remove old non-unique indexes
+    await queryInterface.removeIndex("bookings", "tutor_time_conflict_check");
+    await queryInterface.removeIndex("bookings", "student_time_conflict_check");
+
+    // Add new unique indexes
+    await queryInterface.addIndex(
+      "bookings",
+      ["tutor_id", "scheduled_start", "scheduled_end"],
+      {
+        unique: true,
+        name: "tutor_time_conflict_check",
+      }
+    );
+
+    await queryInterface.addIndex(
+      "bookings",
+      ["student_id", "scheduled_start", "scheduled_end"],
+      {
+        unique: true,
+        name: "student_time_conflict_check",
+      }
+    );
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.changeColumn("Bookings", "reminders", {
-      type: Sequelize.JSONB,
-      allowNull: false,
-      defaultValue: { "24h": false, "1h": false, "15m": false },
-    });
+    // Revert back to non-unique indexes
+    await queryInterface.removeIndex("bookings", "tutor_time_conflict_check");
+    await queryInterface.removeIndex("bookings", "student_time_conflict_check");
+
+    await queryInterface.addIndex(
+      "bookings",
+      ["tutor_id", "scheduled_start", "scheduled_end"],
+      {
+        name: "tutor_time_conflict_check",
+      }
+    );
+
+    await queryInterface.addIndex(
+      "bookings",
+      ["student_id", "scheduled_start", "scheduled_end"],
+      {
+        name: "student_time_conflict_check",
+      }
+    );
   },
 };
