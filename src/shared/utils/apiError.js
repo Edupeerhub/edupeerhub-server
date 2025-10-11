bb178d9eb0e0692c385c7f4210667cf3d279c2ab
Abisoye Ogunmona <Ogunmonaa@gmail.com>
2025-10-11 22:03:38 +0100

    fix missing stack trace in error logs

--- src/shared/utils/apiError.js
+++ src/shared/utils/apiError.js
@@ -1,9 +1,20 @@
 class ApiError extends Error {
-  constructor(message, statusCode = 500, errorDetails = null) {
+  constructor(
+    message,
+    statusCode = 500,
+    errorDetails = null,
+    originalError = null
+  ) {
     super(message);
+    this.name = this.constructor.name;
     this.statusCode = statusCode;
     this.details = errorDetails;
-    Error.captureStackTrace(this, this.constructor);
+
+    if (originalError && originalError.stack) {
+      this.stack = originalError.stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
   }
 }
 
